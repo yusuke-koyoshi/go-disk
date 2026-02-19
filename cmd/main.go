bd9e3f642628232df148b2ae716576dffc75e3ef
yusuke.koyoshi <yusuke.koyoshi@assured.inc>
2026-02-19 13:13:46 +0900

    feat: add partition skip logic and ls -l style file listing to disk walker
    Align cmd/main.go with Trivy's VM scanning approach:
    - Skip boot/empty/non-target partitions with log messages
    - Add ext4 filesystem support alongside xfs
    - Use "/" as WalkDir root for ext4 library compatibility
    - Convert raw ext4 inode mode to ls -l format (d/l/c/b/p/s/-)

--- cmd/main.go
+++ cmd/main.go
@@ -1,48 +1,168 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
 
 	"github.com/masahiro331/go-disk"
+	"github.com/masahiro331/go-disk/gpt"
+	"github.com/masahiro331/go-disk/types"
+	"github.com/masahiro331/go-ext4-filesystem/ext4"
 	"github.com/masahiro331/go-xfs-filesystem/xfs"
 )
 
 func main() {
 	if len(os.Args) != 2 {
-		log.Fatal("arguments error, './main ${file}'")
+		log.Fatal("usage: ./main <disk image>")
 	}
 	f, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
+
 	fi, err := f.Stat()
 	if err != nil {
 		log.Fatal(err)
 	}
-	r := io.NewSectionReader(f, 0, fi.Size())
-	driver, err := disk.NewDriver(r, xfs.Check)
+	sr := io.NewSectionReader(f, 0, fi.Size())
+
+	driver, err := disk.NewDriver(sr, ext4.Check, xfs.Check)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	count := 0
 	for {
-		p, err := driver.Next()
+		partition, err := driver.Next()
 		if err != nil {
 			if err == io.EOF {
-				return
+				break
 			}
-			log.Fatal(p.Name(), err)
-		}
-		f, err = os.Create(fmt.Sprintf("%s%d", p.Name(), count))
-		if err != nil {
 			log.Fatal(err)
 		}
-		r := p.GetSectionReader()
-		io.Copy(f, &r)
-		count++
+
+		if shouldSkip(partition) {
+			log.Printf("skipping partition %s: not a target partition", partition.Name())
+			continue
+		}
+
+		fmt.Printf("=== Partition: %s ===\n", partition.Name())
+
+		psr := partition.GetSectionReader()
+
+		fsys, err := newFS(psr)
+		if err != nil {
+			fmt.Printf("  skipping: %v\n", err)
+			continue
+		}
+
+		err = fs.WalkDir(fsys, "/", func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				log.Printf("walk error at %s: %v", path, err)
+				return nil
+			}
+			info, err := d.Info()
+			if err != nil {
+				return nil
+			}
+			fmt.Printf("  %s\t%d\t%s\n", formatMode(info.Mode()), info.Size(), path)
+			return nil
+		})
+		if err != nil {
+			fmt.Printf("  walk error: %v\n", err)
+		}
 	}
+}
+
+type fsFactory struct {
+	check func(io.Reader) bool
+	newFS func(io.SectionReader) (fs.FS, error)
+}
+
+var fsFactories = []fsFactory{
+	{check: ext4.Check, newFS: func(sr io.SectionReader) (fs.FS, error) { return ext4.NewFS(sr, nil) }},
+	{check: xfs.Check, newFS: func(sr io.SectionReader) (fs.FS, error) { return xfs.NewFS(sr, nil) }},
+}
+
+var requiredDiskName = []string{
+	"Linux",
+	"p.lxroot",
+	"primary",
+	"ROOT",
+	"0", "1", "2", "3",
+}
+
+func shouldSkip(partition types.Partition) bool {
+	if bytes.Equal(partition.GetType(), []byte{0x00}) {
+		return true
+	}
+
+	found := false
+	for _, name := range requiredDiskName {
+		if partition.Name() == name {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return true
+	}
+
+	if p, ok := partition.(*gpt.PartitionEntry); ok {
+		return p.Bootable()
+	}
+	return false
+}
+
+func formatMode(mode fs.FileMode) string {
+	// ext4 の i_mode がそのまま fs.FileMode にキャストされているため、
+	// 上位ビットから ext4 のファイルタイプを取得して ls -l 形式に変換する
+	raw := uint16(mode)
+	var typeChar byte
+	switch raw & 0xF000 {
+	case 0x4000:
+		typeChar = 'd'
+	case 0xA000:
+		typeChar = 'l'
+	case 0x2000:
+		typeChar = 'c'
+	case 0x6000:
+		typeChar = 'b'
+	case 0x1000:
+		typeChar = 'p'
+	case 0xC000:
+		typeChar = 's'
+	default:
+		typeChar = '-'
+	}
+
+	perm := raw & 0x1FF
+	buf := [10]byte{typeChar, '-', '-', '-', '-', '-', '-', '-', '-', '-'}
+	const rwx = "rwx"
+	for i := 0; i < 9; i++ {
+		if perm&(1<<uint(8-i)) != 0 {
+			buf[1+i] = rwx[i%3]
+		}
+	}
+	return string(buf[:])
+}
+
+func newFS(sr io.SectionReader) (fs.FS, error) {
+	for _, f := range fsFactories {
+		if _, err := sr.Seek(0, io.SeekStart); err != nil {
+			return nil, err
+		}
+		if !f.check(&sr) {
+			continue
+		}
+		if _, err := sr.Seek(0, io.SeekStart); err != nil {
+			return nil, err
+		}
+		return f.newFS(sr)
+	}
+	return nil, fmt.Errorf("unsupported filesystem")
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/masahiro331/go-disk
 go 1.19
 
 require (
-	github.com/masahiro331/go-xfs-filesystem v0.0.0-20231205045356-1b22259a6c44
+	github.com/masahiro331/go-ext4-filesystem v0.0.0-20240620024024-ca14e6327bbd
+	github.com/masahiro331/go-xfs-filesystem v0.0.0-20250508093807-355a81f6ae2f
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
 )
 
 require (
+	github.com/lunixbochs/struc v0.0.0-20200707160740-784aaebc1d40 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
